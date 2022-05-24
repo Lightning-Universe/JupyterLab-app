e5a296b68861b80d066d1724f25c00d8dfb01519
thomas chaton <thomas@grid.ai>
2022-05-24 19:26:32 -0400

    update

--- app.py
+++ app.py
@@ -1,65 +1,74 @@
-from typing import Dict, List
-from lightning.utilities.packaging.cloud_compute import CloudCompute
-from jupyter_work import JupyterWork
-from lightning.frontend import StreamlitFrontend
-from lightning import LightningApp, LightningFlow
-from lightning.utilities.state import AppState
-from lightning.utilities.network import find_free_network_port
+import lightning as L
+from lightning.components.python import JupyterLabWork
 
-
-class JupyterLabManager(LightningFlow):
+class JupyterLabManager(L.LightningFlow):
 
     def __init__(self):
         super().__init__()
-        self.jupyter_configs = []
+        self.notebooks_session = L.structures.Dict()
+        self.user_notebook_configs = []
 
     def run(self):
-        for jupyter_idx, jupyter_config in enumerate(self.jupyter_configs):
-            name = f"jupyter_work_{jupyter_idx}"
-            if not hasattr(self, name):
+        for jupyter_config in self.user_notebook_configs:
+
+            username = jupyter_config["username"]
+            if username not in self.notebooks_session:
                 jupyter_config["ready"] = False
                 accelerator = "gpu" if jupyter_config["use_gpu"] else "cpu"
-                setattr(self, name, JupyterWork(cloud_compute=CloudCompute(accelerator, 1), port=find_free_network_port()))
-            jupyter_work = getattr(self, name)
-            jupyter_work.run()
-            if jupyter_work.token:
-                jupyter_config["token"] = jupyter_work.token
+                self.notebooks_session[username] = JupyterLabWork(cloud_compute=L.CloudCompute(accelerator, 1))
+            self.notebooks_session[username].run()
 
+            if self.notebooks_session[username].token:
+                jupyter_config["token"] = self.notebooks_session[username].token
+
+            if jupyter_config['stop']:
+                self.notebooks_session[username].run()
 
     def configure_layout(self):
-        return StreamlitFrontend(render_fn=render_fn)
+        return L.frontend.StreamlitFrontend(render_fn=render_fn)
 
-def render_fn(state: AppState):
+def render_fn(state):
     import streamlit as st
 
-    col1, col2 = st.columns(2)
+    st.set_page_config(layout="wide")
+
+    col1, col2, col3 = st.columns(3)
     with col1:
        create_jupyter = st.button("Create Jupyter Notebook")
     with col2:
+        username = st.text_input('Enter your username', "tchaton")
+        assert username
+    with col3:
         use_gpu = st.checkbox('Use GPU')
 
     if create_jupyter:
-        state.jupyter_configs = state.jupyter_configs + [{"use_gpu": use_gpu, "token": None}]
+        state.user_notebook_configs = state.user_notebook_configs + [{"use_gpu": use_gpu, "token": None, "username": username, "stop": False}]
 
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
     with col1:
         st.write(f"Idx")
     with col2:
         st.write(f"Use GPU")
     with col3:
         st.write(f"Token")
+    with col4:
+        st.write(f"Stop")
 
-    for idx, config in enumerate(state.jupyter_configs):
-        col1, col2, col3 = st.columns(3)
+    for idx, config in enumerate(state.user_notebook_configs):
+        col1, col2, col3, col4 = st.columns(4)
         with col1:
             st.write(f"{idx}")
         with col2:
             st.write(config['use_gpu'])
         with col3:
             st.write(config["token"])
+        with col4:
+            if config["token"]:
+                should_stop = st.button("Do you want to stop the notebook")
+                if should_stop:
+                    config["stop"] = should_stop
 
-
-class RootFlow(LightningFlow):
+class RootFlow(L.LightningFlow):
 
     def __init__(self):
         super().__init__()
@@ -68,15 +77,13 @@ class RootFlow(LightningFlow):
     def run(self):
         self.manager.run()
 
-    def configure_layout(self) -> List[Dict]:
+    def configure_layout(self):
         layout = [{"name": "Manager", "content": self.manager}]
-        for idx, work in enumerate(self.manager.works()):
-            jupyter_url = work.url
-            jupyter_url = jupyter_url + "/lab" if jupyter_url else jupyter_url
+        for username, jupyter_work in self.manager.notebooks_session.items():
             layout.append(
-                {"name": f"JupyterLab {idx}", "content": jupyter_url}
+                {"name": f"JupyterLab {username}", "content": jupyter_work.url + "/lab"}
             )
         return layout
 
 
-app = LightningApp(RootFlow())
+app = L.LightningApp(RootFlow())
